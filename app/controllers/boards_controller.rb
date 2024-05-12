4778cd3de5ad51f7adf9e5307ec027eeba7d939d
abyssekk <nokhrok2024@gmail.com>
2024-05-12 21:42:07 +0900

    url link to title and body

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,32 +1,51 @@
 class BoardsController < ApplicationController
     def new
         @board = Board.new
+        if session[:url] then
+            @board = Board.new(title: session[:url], body: session[:url], url: session[:url])
+            session.delete(:url)
+        end
     end
 
     def create
-        @board = current_user.boards.build(board_params)
-        print(board_params[:title])
-        if @board.save
-            redirect_to boards_path
-            flash[:success] = t('.success')
+        if params[:register] then
+            @board = current_user.boards.build(board_params)
+            if @board.save
+                redirect_to boards_path
+                flash[:success] = t('.success')
+            else
+                flash.now[:danger] = t('.fail')
+                render :new
+            end
         else
-            flash.now[:danger] = t('.fail')
-            render :new
+            session[:url] = board_params[:url]
+            redirect_to new_board_path
         end
     end
 
     def edit
         @board = current_user.boards.find(params[:id])
+        if session[:url] then
+            @board.url = session[:url]
+            @board.title = session[:url]
+            @board.body = session[:url]
+            session.delete(:url)
+        end
     end
 
     def update
-        @board = Board.find(params[:id])
-        if @board.update(board_params)
-            redirect_to board_path(@board)
-            flash[:success] = t('defaults.updated')
+        if params[:register] then
+            @board = Board.find(params[:id])
+            if @board.update(board_params)
+                redirect_to board_path(@board)
+                flash[:success] = t('defaults.updated')
+            else
+                flash.now[:danger] = t('defaults.not_updated')
+                render :new
+            end
         else
-            flash.now[:danger] = t('defaults.not_updated')
-            render :new
+            session[:url] = board_params[:url]
+            redirect_to edit_board_path
         end
     end
 
@@ -48,7 +67,7 @@ class BoardsController < ApplicationController
     private
 
     def board_params
-        params.require(:board).permit(:title, :body)
+        params.require(:board).permit(:title, :body, :url)
     end
 
     def extract
