f37a1d7df5b1b2954cf7451575ecb8aa56e66445
abyssekk <nokhrok2024@gmail.com>
2024-07-14 18:07:15 +0900

    for deploy minor fix

--- db/migrate/20240608102557_add_columns_users.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsBoards < ActiveRecord::Migration[5.2]
-    def change
-      add_column :users, :nickname, :string
-      remove_column :users, :last_name, :string
-      remove_column :users, :first_name, :string
-    end
-  end
