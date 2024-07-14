a04062b534ec9b05630261cc0a266207585051da
abyssekk <nokhrok2024@gmail.com>
2024-07-14 18:07:45 +0900

    for deploy minor fix2

---
+++ db/migrate/20240608102557_add_cloumn_users.rb
@@ -0,0 +1,7 @@
+class AddCloumnUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :nickname, :string
+    remove_column :users, :last_name, :string
+    remove_column :users, :first_name, :string
+  end
+end
