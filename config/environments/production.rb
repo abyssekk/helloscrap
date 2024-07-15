f40df12b99691d8e4eee1cc04ed0473f31c77852
abyssekk <nokhrok2024@gmail.com>
2024-07-15 19:57:04 +0900

    for deploy minor fix11

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,9 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = Settings.default_url_options.to_h
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
@@ -79,6 +77,9 @@ Rails.application.configure do
     authentication: :plain,
     enable_starttls_auto: true 
   }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = Settings.default_url_options.to_h
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
