cc0797c2dc431510ac3263f9e334df4b6ef7b02f
abyssekk <nokhrok2024@gmail.com>
2024-07-15 20:13:54 +0900

    for deploy minor fix12

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'from@example.com'
+  default from: ENV['GMAIL_USERNAME']
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
